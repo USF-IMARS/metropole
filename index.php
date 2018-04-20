bf02082748decf8720a4f54419af2c1dbae8eb0b
cjrmbon <cjreynolds@usf.edu>
2018-04-20 17:19:20 -0400

    Add FFMA copy and increase headers to 14 Pt
    Also link to new PDF FFMA presentation

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 		</div>
 
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-14">
 				<p><strong>Read Our Latest Articles</strong></p>
 				<p>
 					Merrill, S., Kartez, J., Langbehn,K., Muller Karger F., Reynolds, C., 2018 
@@ -70,9 +70,12 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-12">
-				<p><strong>Presentations for Elected Officials</strong></p>
-				<p>
+			<div class="col-md-14">
+				<p><strong>Latest Presentations for Practitioners and Elected Officials</strong></p>
+				<p> 
+				<p>Florida Floodplain Managers Association Annual Conference, Orlando, April 12, 2018 </p>
+				<a href="files/USF FFMA 2018 upload.pdf">
+					
 					<a href="http://www.floridaleagueofcities.com/docs/default-source/CMRI/Dec-17-Presentations/reynolds-flc-2017.pdf?sfvrsn=4" >
 						"Understanding Community Values to Support Resiliency Improvements," 
 					</a>
@@ -82,17 +85,14 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-12">
-				<p><strong>Latest Presentation for Practitioners</strong></p>
-				<p>American Planning Association -- Florida Chapter</p>
-				<p>Annual Conference session Sept. 7, 8:30-9:45, 2017</p>
-				<p>
-					<a href="files/Sept 7 2017 830 METROPOLE.pdf">"Working with Social Factors and Values to Support Adaptation Planning and Public Finance"</a>
+				<p>American Planning Association -- Florida Chapter, Annual Conference session Sept. 7, 8:30-9:45, 2017</p>
+				<a href="files/Sept 7 2017 830 METROPOLE.pdf">"Working with Social Factors and Values to Support Adaptation Planning and Public Finance"</a>
 				</p>
-			</div>
+	</p>		
+	</div>
 		</div>
 		<div class="row">  <!-- sub-footer -->
-			<div class="col-md-12">
+			<div class="col-md-14">
 				<p><strong>Collaborations between universities, private sector and local government deliver tangible benefits to society – locally and globally.</strong></p>
 
 				<img class="img-responsive" alt="team logo" src="image/partner_logos.jpg" style="width:936px">
