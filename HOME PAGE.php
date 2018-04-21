5034f987db72cdd53a90855e3e5a9eaa9c3b7727
Tylar <murray.tylar@gmail.com>
2018-04-21 17:25:11 +0000

    improvements to formatting & indentation

--- HOME PAGE.php
+++ HOME PAGE.php
@@ -5,29 +5,71 @@
 <!-- End WOWSlider.com HEAD section -->
 <!-- Start WOWSlider.com BODY section -->
 <div id="wowslider-container1">
-<div class="ws_images"><ul>
-		<li><img src="data1/images/banner_fin_workshops.jpg" alt="Multiple stakeholder workshops in each country" title="Multiple stakeholder workshops in each country" id="wows1_0"/></li>
-		<li><img src="data1/images/banner_fin_areas.jpg" alt="Broward County, Florida, Santos, Brazil and Selsey, UK" title="Broward County, Florida, Santos, Brazil and Selsey, UK" id="wows1_1"/></li>
-		<li><img src="data1/images/banner_fin_city_logos.jpg" alt="Active municipal partners in Brazil, UK and US
-" title="Active municipal partners in Brazil, UK and US
-" id="wows1_2"/></li>
-		<li><a href="http://wowslider.com"><img src="data1/images/banner_fin_research.jpg" alt="bootstrap carousel" title="Calculating sea level rise damage estimates" id="wows1_3"/></a></li>
-		<li><img src="data1/images/banner_fin_people.jpg" alt="Identifying values that influence adaptation planning" title="Identifying values that influence adaptation planning" id="wows1_4"/></li>
+	<div class="ws_images"><ul>
+		<li><img src="data1/images/banner_fin_workshops.jpg" 
+				 alt="Multiple stakeholder workshops in each country" 
+				 title="Multiple stakeholder workshops in each country" 
+				 id="wows1_0"
+		 /></li>
+		<li><img src="data1/images/banner_fin_areas.jpg" 
+				 alt="Broward County, Florida, Santos, Brazil and Selsey, UK" 
+				 title="Broward County, Florida, Santos, Brazil and Selsey, UK" 
+				 id="wows1_1"
+		/></li>
+		<li><img src="data1/images/banner_fin_city_logos.jpg" 
+				 alt="Active municipal partners in Brazil, UK and US" 
+				 title="Active municipal partners in Brazil, UK and US" 
+				 id="wows1_2"
+		/></li>
+		<li><a href="http://wowslider.com"><img src="data1/images/banner_fin_research.jpg"
+			alt="bootstrap carousel" 
+			title="Calculating sea level rise damage estimates" 
+			id="wows1_3"
+		/></a></li>
+		<li><img src="data1/images/banner_fin_people.jpg"
+				 alt="Identifying values that influence adaptation planning" 
+				 title="Identifying values that influence adaptation planning" 
+				 id="wows1_4"
+		/></li>
 	</ul></div>
-	<div class="ws_bullets"><div>
-		<a href="#" title="Multiple stakeholder workshops in each country"><span><img src="data1/tooltips/banner_fin_workshops.jpg" alt="Multiple stakeholder workshops in each country"/>1</span></a>
-		<a href="#" title="Broward County, Florida, Santos, Brazil and Selsey, UK"><span><img src="data1/tooltips/banner_fin_areas.jpg" alt="Broward County, Florida, Santos, Brazil and Selsey, UK"/>2</span></a>
-		<a href="#" title="Active municipal partners in Brazil, UK and US
-"><span><img src="data1/tooltips/banner_fin_city_logos.jpg" alt="Active municipal partners in Brazil, UK and US
-"/>3</span></a>
-		<a href="#" title="Calculating sea level rise damage estimates"><span><img src="data1/tooltips/banner_fin_research.jpg" alt="Calculating sea level rise damage estimates"/>4</span></a>
-		<a href="#" title="Identifying values that influence adaptation planning"><span><img src="data1/tooltips/banner_fin_people.jpg" alt="Identifying values that influence adaptation planning"/>5</span></a>
-	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">http://wowslider.com/</a> by WOWSlider.com v8.7.1m</div>
-<div class="ws_shadow"></div>
+	<div class="ws_bullets">
+		<div>
+			<a href="#" title="Multiple stakeholder workshops in each country">
+				<span><img src="data1/tooltips/banner_fin_workshops.jpg" 
+						alt="Multiple stakeholder workshops in each country"
+				/>1</span>
+			</a>
+			<a href="#" title="Broward County, Florida, Santos, Brazil and Selsey, UK">
+				<span><img src="data1/tooltips/banner_fin_areas.jpg" 
+				alt="Broward County, Florida, Santos, Brazil and Selsey, UK"
+			/>2</span></a>
+			<a href="#" title="Active municipal partners in Brazil, UK and US"><span>
+				<img src="data1/tooltips/banner_fin_city_logos.jpg" 
+					 alt="Active municipal partners in Brazil, UK and US"
+				/>
+				3
+			</span></a>
+			<a href="#" title="Calculating sea level rise damage estimates"><span>
+				<img src="data1/tooltips/banner_fin_research.jpg" alt="Calculating sea level rise damage estimates"/>
+				4
+			</span></a>
+			<a href="#" title="Identifying values that influence adaptation planning"><span>
+				<img src="data1/tooltips/banner_fin_people.jpg" 
+					 alt="Identifying values that influence adaptation planning"
+				/>
+				5
+			</span></a>
+		</div>
+	</div>
+	<div class="ws_script" style="position:absolute;left:-99%">
+		<a href="http://wowslider.com">http://wowslider.com/</a> by WOWSlider.com v8.7.1m
+	</div>
+	<div class="ws_shadow"></div>
 </div>
 <script type="text/javascript" src="engine1/wowslider.js"></script>
 <script type="text/javascript" src="engine1/script.js"></script>
 <!-- End WOWSlider.com BODY section -->
+
 <div class="row">
 	<div class="col-md-12">
 		<!-- old slides -->
@@ -39,16 +81,38 @@
 			<img class="mySlides" src="image/slide/banner_fin_people.jpg" style="width:100%">
 		</div> -->
 		<h1>Advancing International Adaptation Research and Local Planning Methods in Coastal Communities  </h1>
-		<p>Coastal communities – from small towns to major metropolis -- face increasing management and fiscal challenges. Innovations in collaborative research action are needed to transform governance and community engagement practices to better mobilize our communities.</p>
+		<p>
+			Coastal communities – from small towns to major metropolis -- face increasing management and fiscal challenges. 
+			Innovations in collaborative research action are needed to transform governance and community engagement practices 
+			to better mobilize our communities.
+		</p>
 
 		<div class="row">
 			<div class="col-md-9">
-				<p>The international METROPOLE research project funded by the <a href="research.php#belmont">Belmont Forum</a> brought together <a href="team.php">social, economic, environmental scientists</a> and municipal practitioners and officials to assess the socio-economic and cultural factors which create potential barriers to adaptation, and influence <a href="research.php#aci">adaptive capacity</a> among local and regional institutions in three areas in Brazil, the UK and the US.</p>
-				<p>Working with local decision makers, technical staff and stakeholder leaders in three study areas encompassing six cities, Metropole researchers evaluated the vulnerability to sea level rise and flooding with COAST and develop cost assessments of risk over time and developed costs-benefits analysis for chosen adaptation actions.</p>
-				<p>The process observations and <a href="research.php#survey">survey results</a> are producing new insights relevant for local administrators, engineers and others involved in land-use planning. New perspectives on the links between values, type of infrastructure improvement and acceptability of public finance mechanisms will improve community support for large scale capital improvement projects.</p>
+				<p>
+					The international METROPOLE research project funded by the <a href="research.php#belmont">Belmont Forum</a> 
+					brought together <a href="team.php">social, economic, environmental scientists</a> and municipal 
+					practitioners and officials to assess the socio-economic and cultural factors which create potential 
+					barriers to adaptation, and influence <a href="research.php#aci">adaptive capacity</a> among local and 
+					regional institutions in three areas in Brazil, the UK and the US.
+				</p>
+				<p>
+					Working with local decision makers, technical staff and stakeholder leaders in three study areas 
+					encompassing six cities, Metropole researchers evaluated the vulnerability to sea level rise and flooding 
+					with COAST and develop cost assessments of risk over time and developed costs-benefits analysis for 
+					chosen adaptation actions.
+				</p>
+				<p>
+					The process observations and <a href="research.php#survey">survey results</a> are producing new insights 
+					relevant for local administrators, engineers and others involved in land-use planning. 
+					New perspectives on the links between values, type of infrastructure improvement and acceptability of 
+					public finance mechanisms will improve community support for large scale capital improvement projects.
+				</p>
 			</div>
 			<div class="col-md-3">
-				<a href="files/Belmont Synthesis_20March.pdf" target="_blank"><img src="image/belmont_workshop.jpg" alt="Belmont Workshop" class="img-responsive"></a>
+				<a href="files/Belmont Synthesis_20March.pdf" target="_blank">
+					<img src="image/belmont_workshop.jpg" alt="Belmont Workshop" class="img-responsive"/>
+				</a>
 			</div>
 		</div>
 
@@ -64,36 +128,58 @@
 				<p>
 					<a href="http://file.scirp.org/Html/11-2360499_77255.htm">An Integrated Framework to Analyze Local Decision Making and Adaptation to Sea Level Rise in Coastal Regions in Selsey (UK), Broward County (USA), and Santos (Brazil).</a> American Journal of Climate Change, 2017.
 				</p>
-				<p><a href="https://link.springer.com/article/10.1007/s11069-017-2855-x" target="_blank">A Globally-Deployable Strategy for Co-Development of Adaptation Preferences to Sea-Level Rise: The Public Participation Case of Santos, Brazil</a>, <i>Natural Hazards</i>, April 2017.</p>
-				<p><a href="http://www.sciencedirect.com/science/article/pii/S0016718517300507" target="_blank">Size does matter: city scale and the asymmetries of climate change adaptation in three coastal towns</a>, <i>Geoforum Journal</i>, January 2017.</p>
-				<p><a href="http://cbe.miis.edu/joce/vol3/iss2/4/" target="_blank">Climate Adaptation Finance Mechanisms: New Frontiers For Familiar Tools</a>, <i>Journal of Ocean and Coastal Economics</i>.</p>
+				<p>
+					<a href="https://link.springer.com/article/10.1007/s11069-017-2855-x" target="_blank">
+						A Globally-Deployable Strategy for Co-Development of Adaptation Preferences to Sea-Level Rise: 
+						The Public Participation Case of Santos, Brazil
+					</a>, <i>Natural Hazards</i>, April 2017.
+				</p>
+				<p>
+					<a href="http://www.sciencedirect.com/science/article/pii/S0016718517300507" target="_blank">
+						Size does matter: city scale and the asymmetries of climate change adaptation in three coastal towns
+					</a>, <i>Geoforum Journal</i>, January 2017.
+				</p>
+				<p>
+					<a href="http://cbe.miis.edu/joce/vol3/iss2/4/" target="_blank">
+						Climate Adaptation Finance Mechanisms: New Frontiers For Familiar Tools
+					</a>, <i>Journal of Ocean and Coastal Economics</i>.
+				</p>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-12">
 				<h4><strong>Latest Presentations for Practitioners and Elected Officials</strong></h4>
-				<p> 
-					<p>Florida Floodplain Managers Association Annual Conference, Orlando, April 12, 2018 </p>
-					<a href="files/USF FFMA 2018 upload.pdf">
-					
-					<a href="http://www.floridaleagueofcities.com/docs/default-source/CMRI/Dec-17-Presentations/reynolds-flc-2017.pdf?sfvrsn=4" >
+				<div>
+					<p>
+						<a href="files/USF FFMA 2018 upload.pdf">
+							Florida Floodplain Managers Association Annual Conference, Orlando, April 12, 2018
+						</a>
+					</p>
+					<p>
+						<a href="http://www.floridaleagueofcities.com/docs/default-source/CMRI/Dec-17-Presentations/reynolds-flc-2017.pdf?sfvrsn=4">
 						"Understanding Community Values to Support Resiliency Improvements," 
-					</a>
-					Reynolds, C.J., Keynote Address, 
-					Florida League of Cities Annual Research Symposium, Dec. 2017, Orlando, FL
-				</p>
+						</a>
+						Reynolds, C.J., Keynote Address, 
+						Florida League of Cities Annual Research Symposium, Dec. 2017, Orlando, FL
+					</p>
+				</div>
 			</div>
 		</div>
 		<div class="row">
-				<p>American Planning Association -- Florida Chapter, Annual Conference session Sept. 7, 8:30-9:45, 2017</p>
-				<a href="files/Sept 7 2017 830 METROPOLE.pdf">"Working with Social Factors and Values to Support Adaptation Planning and Public Finance"</a>
-				</p>
+			<p>
+				American Planning Association -- Florida Chapter, Annual Conference session Sept. 7, 8:30-9:45, 2017
+				<a href="files/Sept 7 2017 830 METROPOLE.pdf">
+					"Working with Social Factors and Values to Support Adaptation Planning and Public Finance"
+				</a>
+			</p>
 		</div>
 		<div class="row">  <!-- sub-footer -->
 			<div class="col-md-12">
-				<h4><strong>Collaborations between universities, private sector and local government deliver tangible benefits to society – locally and globally.</strong></h4>
-
-				<img class="img-responsive" alt="team logo" src="image/partner_logos.jpg" style="width:936px">
+				<h4><strong>
+					Collaborations between universities, private sector and local government deliver tangible benefits to 
+					society – locally and globally.
+				</strong></h4>
+				<img class="img-responsive" alt="team logo" src="image/partner_logos.jpg" style="width:936px"/>
 			</div>
 		</div>  <!-- / row sub-footer -->
 	</div>
